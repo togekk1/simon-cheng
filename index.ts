7f10a089a1ef8aa1b7e7d14dd89a552eb32b8358
togekk1 <togekk@gmail.com>
2020-03-14 13:46:04 +0800

    Setting up environment

---
+++ index.ts
@@ -0,0 +1,8 @@
+// declare const require: Function;
+// declare const __dirname: string;
+
+// const fs = require("fs");
+// const loader = require("@assemblyscript/loader");
+// const wasm = loader.instantiateSync(fs.readFileSync(__dirname + "/build/optimized.wasm"), { /* imports */ })
+
+console.log('OK');
