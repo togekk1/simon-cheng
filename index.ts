0d9daa025bb0b272adb5c1802057b2ca96d313cc
Simon.Cheng <Simon.Cheng@advantech.com>
2020-03-28 12:37:02 +0800

    Roll back to yarn v1

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
-// declare const require: Function;
-// declare const __dirname: string;
-
-// const fs = require("fs");
-// const loader = require("@assemblyscript/loader");
-// const wasm = loader.instantiateSync(fs.readFileSync(__dirname + "/build/optimized.wasm"), { /* imports */ })
-
-console.log('OK');
+import * as loader from '@assemblyscript/loader';
+(async (): Promise<void> => {
+    try {
+        const wasm = await loader.instantiateStreaming(fetch('build/optimized.wasm'), { /* imports */ })
+        console.log(wasm);
+    }
+    catch (err) {
+        console.error(err);
+    }
+})
