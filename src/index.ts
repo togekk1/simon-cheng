0f65437e157cbeab503dc245e964ac79744aad4c
Simon.Cheng <Simon.Cheng@advantech.com>
2020-03-28 19:33:21 +0800

    Move home page to home folder

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import wasm from './wasm';
-// @ts-ignore
-import wasm_html from './pages/home/home.html';
+import home from './pages/home/home';
 
 class app {
     wasm: wasm = new wasm();
@@ -8,11 +7,7 @@ class app {
 
     async init() {
         await this.wasm.load();
-        const { alloc_string }: wasm = this.wasm,
-            click_event_str: string = wasm_html.match(/\(click\)="(.*)"/gm)[0],
-            click_event: string = click_event_str.replace('(click)=\"', '').replace('\"', ''),
-            wasm_html_new = wasm_html.replace(click_event_str, '');
-        this.wasm.main(alloc_string(wasm_html_new), alloc_string(click_event));
+        new home(this.wasm);
     }
 }
 
